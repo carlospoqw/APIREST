c102d845185f0d26d0251de7868c533ffb56dd93
Carlos Alexis Aguirre Castillo <carlospoqw@gmail.com>
2019-09-08 16:49:38 -0300

    add the crud

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,27 +1,71 @@
 const { Router } = require('express');
 const router = Router();
+const _ = require('underscore');
+var movies = require('../sample.json');
 
-const movies = require('../sample.json');
-
+//Get the array
 router.get('/', (req, res) => {
     res.json(movies);
 });
 
+//Add one
 router.post('/', (req, res) => {
-    const { title, director, year, rating} = req.body;
-    if(title && director && year && rating){
-        var id = movies.length + 1;
-        id=id.toString();
-        const newMovie ={id,...req.body};
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        var UsedID = 1;
+        _.each(movies, (movie, i) => {
+            if (UsedID == parseInt(movie.id)) {
+                UsedID += 1;
+            }
+        });
+        UsedID = UsedID.toString();
+        const newMovie = { UsedID, ...req.body };
         movies.push(newMovie);
         res.json(movies);
-    }else{
-        res.status(500).json({error:'You miss something.'});
+    } else {
+        res.status(500).json({ error: 'You miss something.' });
     }
 });
 
-router.delete('/', (req, res) => {
-    res.json(movies);
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(movies, (movie, i) => {
+        if (movie.id == id) {
+            movies.splice(i, 1);
+            res.send(movies);
+        }
+    })
+    res.send(movies);
+});
+
+//Update one
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    //If Exist
+    var exist = false;
+    _.each(movies, (movie, i) => {
+        if (movie.id == id) {
+            exist = true;
+        }
+    });
+    //Change things
+    if (title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+                res.json(movies);
+            }
+        })
+    } else {
+        res.status(500).json({ error: 'You miss something.' });
+    }
+    if (!exist) {
+        res.status(500).json({ error: 'Not Exist.' });
+    };
 });
 
 module.exports = router;
