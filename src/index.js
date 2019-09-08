384e2ff3bccdc57cd0d08ad379729c685a7abaaa
Carlos Alexis Aguirre Castillo <carlospoqw@gmail.com>
2019-09-08 17:01:59 -0300

    added getting json from url

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
 // server Settings
-app.set('port',process.env.PORT || 8080);
-app.set('json spaces',2);
+app.set('port', process.env.PORT || 8080);
+app.set('json spaces', 2);
 
 // middlewares
 app.use(morgan('combined'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // routes
 app.use(require('./routes/index'));
-app.use('/api/movies',require('./routes/movies'));
+app.use('/api/movies', require('./routes/movies'));
+app.use('/api/users', require('./routes/users'));
 
 // starting the server
 app.listen(app.get('port'), () => {
-    console.log('App listening on port '+app.get('port'));
+    console.log('App listening on port ' + app.get('port'));
 });
